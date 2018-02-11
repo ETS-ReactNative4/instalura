ed7487bb420f05a48c5b7cfd359a23757f415114
Thiago Resende <thiago.gcresende@gmail.com>
2018-02-11 00:11:17 +0000

    React-Redux

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ import Logout from './componentes/Logout';
 import { Router, Route, browserHistory } from 'react-router'
 import { matchPattern } from 'react-router/lib/PatternUtils'
 
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { timeline } from './reducers/timeline'
+import { notificacao } from './reducers/header'
+import { Provider } from 'react-redux';
+
+const reducers = combineReducers({ timeline, notificacao });
+const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+
 function verificaAutenticacao(nextState, replace) {
   const resultado = matchPattern('/timeline(/:login)', nextState.location.pathname);
   const enderecoPrivadoTimeline = resultado.paramValues[0] === undefined;
@@ -19,10 +28,12 @@ function verificaAutenticacao(nextState, replace) {
 
 ReactDOM.render(
   (
-    <Router history={browserHistory}>
-      <Route path="/" component={Login} />
-      <Route path="/timeline(/:login)" component={App} onEnter={verificaAutenticacao} />
-      <Route path="/logout" component={Logout} />
-    </Router>
+    <Provider store={store}>
+      <Router history={browserHistory}>
+        <Route path="/" component={Login} />
+        <Route path="/timeline(/:login)" component={App} onEnter={verificaAutenticacao} />
+        <Route path="/logout" component={Logout} />
+      </Router>
+    </Provider>
   ), document.getElementById('root')
 );
